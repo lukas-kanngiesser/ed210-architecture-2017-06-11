6ce4d7b98e2a22e4963ed47dbfd6583bd1300166
Lukas Kanngießer <lukas.kanngiesser@gmail.com>
2018-06-10 18:37:25 +0200

    biselect other good commit

--- BiSelectExample/src/at/arz/ed210/architecture/biselect/MyService.java
+++ BiSelectExample/src/at/arz/ed210/architecture/biselect/MyService.java
@@ -3,7 +3,7 @@ package at.arz.ed210.architecture.biselect;
 public class MyService {
 
 	public static void main(String[] args) {
-		System.out.println("green, green, broken, green");
+		System.out.println("green, green, broken, green, green");
 		throw new RuntimeException("broken");
 	}
 }
